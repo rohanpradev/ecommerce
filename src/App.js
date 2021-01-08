0a94ba562fe69169a887160dcf05c4888e4b38ff
Rohan <mrohan171093@gmail.com>
2021-01-08 19:08:41 +0530

    added shop comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Route, Switch, Link } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
+
+const NotFoundpage = () => (
+  <div className='not-found'>
+    <h5>Page Not Found</h5>
+    <Link to='/'>Click here to go homepage</Link>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route path='/' exact component={Homepage} />
+      <Route path='/shop' component={Shop} />
+      <Route component={NotFoundpage} />
+    </Switch>
   );
 }
 
