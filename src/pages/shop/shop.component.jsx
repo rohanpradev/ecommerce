bae07e1e21bbe672661fb13596c6f7a74bd49e90
Rohan <mrohan171093@gmail.com>
2021-01-15 22:10:17 +0530

    redux part 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import './shop.styles.scss';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class Shop extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default Shop;
